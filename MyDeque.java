6f94839401f543017218d4f4cd689221b65dfe77
Alvi Aziz <aaziz00@stuy.edu>
2019-03-26 11:02:49 -0400

    code is nicer but error still there :(

--- MyDeque.java
+++ MyDeque.java
@@ -23,68 +23,84 @@ public class MyDeque<E>{
   }
   public String toString(){
     String ans = "{";
-    for (int i = start ; i != (end-1) ; i++){
+    for (int i = start ; i != end+1 ; i++){
       if (i == data.length){
           i = 0;
       }
       if (data[i] != null)
         ans += data[i] + " ";
     }
-    ans += data[(end-1)] + " ";
+    //ans += data[end] + " ";
     return ans.substring(0,ans.length()-1) + "}";
   }
   public void addFirst(E element){
-    start--;
-    if (start == -1 || start == (end+1)){
-      biggerCopy(data,"start");
+    if (size == 0){
+      addLast(element);
     }
-    data[start] = element;
-    size++;
+    else{
+      start--;
+      if (start == -1){
+        start = data.length-1;
+      }
+      if (start == end){
+        biggerCopy(data,"start");
+      }
+      data[start] = element;
+      size++;
+    }
   }
   public void addLast(E element){
-    data[end] = element;
-    end++;
-    size++;
-    if (end == data.length){
-      loop();
+    if (size == 0){
+      data[end] = element;
+      size++;
+      end++;
     }
-    else if (end == start){
-      biggerCopy(data,"end");
+    else{
+      end++;
+      if (end == data.length){
+        loop();
+      }
+      else if (end == start){
+        biggerCopy(data,"start");
+      }
+      data[end] = element;
+      size++;
     }
   }
   public E removeFirst(){
     E b4 = data[start];
     data[start] = null;
-    start++;
     size--;
+    if (start == end){
+      return b4;
+    }
+    start++;
     if (start == data.length)
       start = 0;
     while (data[start] == null){
       start++;
+      if (start == end)
+        return b4;
     }
     return b4;
   }
   public E removeLast(){
-    if ((end-1) != start){
-      E b4 = data[(end-1)];
-      data[(end-1)] = null;
-      end--;
-      size--;
-      if ((end-1) == -1)
-        end = data.length-1;
-      while (data[(end-1)] == null)
-        end--;
+    E b4 = data[end];
+    data[end] = null;
+    size--;
+    if (end == start){
       return b4;
     }
-    E b4 = data[(end-1)];
-    data[(end-1)]=null;
+    end--;
+    if (end == -1)
+      end = start+size-1;
     return b4;
   }
   public E getFirst(){
     return data[start];
   }
   public E getLast(){
-    return data[(end-1)];
+    return data[end];
   }
 
   private void biggerCopy(E[] ary, String mode){
@@ -94,10 +110,12 @@ public class MyDeque<E>{
     if (mode.equals("start")){
       start += ary.length;
     }
-    for (int i = start ; i != (end-1) ; i++){
+
+    for (int i = start ; i != end ; i++){
       if (i == data.length){
           i = 0;
-        }
+          mode = "end";
+      }
       if (mode.equals("start")){
         if (i-ary.length > -1){
           data[i] = ary[i-ary.length];
@@ -107,6 +125,11 @@ public class MyDeque<E>{
         data[i] = ary[i];
       }
     }
+
+    System.out.println("ENDING-1: " + ary[end-1]);
+    System.out.println("ARY LENGTH: " + ary.length);
+    System.out.println("Start index: " + start);
+    System.out.println("End index: " + end);
   }
 
   private void loop(){
@@ -114,7 +137,7 @@ public class MyDeque<E>{
       end = 0;
     }
     else{
-      biggerCopy(data,"(end-1)");
+      biggerCopy(data,"end");
     }
   }
 
--- driver.java
+++ driver.java
@@ -17,15 +17,17 @@ public class driver{
     System.out.println("remove: " + DATA.removeLast());
     DATA.addFirst((Integer)(-3));
     System.out.println("size: " + DATA.size() + "\n" + DATA);
+    System.out.println();
     for (int i = 0 ; i < 18 ; i++){
       DATA.addFirst((Integer)i);
       System.out.println("size: " + DATA.size() + "\n" + DATA);
-      if (i == 4){
-        System.out.println(DATA.getLast());
-      }
+      System.out.println("STARTING VAL: " + DATA.getFirst());
+      System.out.println("ENDING VAL: " + DATA.getLast() + "\n\n");
     }
+    System.out.println(DATA.getLast());
     DATA.addLast((Integer)(-8));
     System.out.println("size: " + DATA.size() + "\n" + DATA);
     System.out.println(DATA.getLast());
+    System.out.println("size: " + DATA.size() + "\n" + DATA);
   }
 }
